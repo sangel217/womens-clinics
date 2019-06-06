2aa01b1db241115c90b85758f4e2787ad1008f99
Student <example@example.com>
2019-06-06 14:17:44 -0700

    add searchby that returns multiple places but still need to fix markers for invidual places

--- src/main.js
+++ src/main.js
@@ -12,49 +12,55 @@ $().ready(function(){
     var map;
     var service;
     var infowindow;
+    var bounds;
     var searchQuery = "seattle";
     
     window.initMap = function() {
-        var location = new google.maps.LatLng();
+        var location = new google.maps.LatLng(47.6, -122.3);
+
             
         infowindow = new google.maps.InfoWindow();
     
         map = new google.maps.Map(
             document.getElementById('map'), {center: location, zoom: 15});
-            
-            //var input = 'Seattle planned parenthood';
     
         var request = {
+            location: location,
+            radius: '500',
             query: searchQuery + " planned parenthood",
             fields: ['name', 'geometry'],
-    
         };
-    
+
         service = new google.maps.places.PlacesService(map);
     
-        service.findPlaceFromQuery(request, function(results, status) {
+        service.nearbySearch(request, function(results, status) {
+            console.log(results);
             if (status === google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
                 createMarker(results[i]);
             }
-            console.log(results[0]);
             map.setCenter(results[0].geometry.location);
-            }
+            }                                                                                                                                                                            
         });
+    
     }
     
-    
-    
-    function createMarker(place) {
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
+    function createMarker(places) {
+        places.forEach(place => {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location,
+                title: place.name
+
+        })
+        location.extend(place.geometry.location);
         });
         
         google.maps.event.addListener(marker, 'click', function() {
             infowindow.setContent(place.name);
             infowindow.open(map, this);
         });
+        map(location);
     }
 
 
